fd7a1137d61459eeb2888709de810e50f1511aba
fitunabu <106749942+fitunabu@users.noreply.github.com>
2023-01-11 15:00:45 +0300

    file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creates a file in /tmp
+
+file { '/tmp/school':
+  ensure  => 'present',
+  content =>  'I love Puppet',
+  owner   =>  'www-data',
+  group   =>  'www-data',
+  mode    =>  '0744'
+}
