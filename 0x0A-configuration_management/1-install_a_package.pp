950357d62b07b407965d511a66b4f9e65ac4c0b6
fitunabu <106749942+fitunabu@users.noreply.github.com>
2023-01-11 15:10:32 +0300

    pip

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# installs puppet-lint
-
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# Install a package
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
 }
