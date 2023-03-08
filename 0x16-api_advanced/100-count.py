a5cf79331bbd16da73bfb2139d129ef621aa7ed8
fitunabu <106749942+fitunabu@users.noreply.github.com>
2023-03-08 10:39:50 +0300

    Count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,46 +1,35 @@
 #!/usr/bin/python3
-"""parses the title of all hot articles, and prints a sorted
-count of given keywords"""
-import requests
+"""
+Show number of occurrences of keywords in hot post titles (case-insensitive)
+"""
 import re
+import requests
+
+API = 'https://www.reddit.com/r/{}/hot.json'
 
 
-def count_words(subreddit, word_list, hot_list=[], after=None):
-    """parses the title of all hot articles"""
-    URL = 'http://reddit.com/r/{}/hot.json'.format(subreddit)
-    HEADERS = {'User-agent': 'Unix:0-subs:v1'}
-    params = {'limit': 100}
-    if isinstance(after, str):
-        if after != "STOP":
-            params['after'] = after
+def count_words(subreddit, wordlist, nums=None, after=None):
+    """
+    Query reddit for hot posts and print total occurrences of each keyword
+    """
+    r = requests.get(
+        API.format(subreddit),
+        headers={'User-Agent': 'Mozilla/5.0'},
+        params={'after': after, 'limit': 100},
+        allow_redirects=False,
+    )
+    if r.status_code == 200:
+        nums = nums or dict.fromkeys(wordlist, 0)
+        data = r.json()['data']
+        page = [word for post in data['children']
+                for word in post['data']['title'].split()]
+        for key in wordlist:
+            for word in page:
+                if key.casefold() == word.casefold():
+                    nums[key] += 1
+        if data['after'] is None:
+            keys = sorted(filter(nums.get, nums), key=lambda k: (-nums[k], k))
+            for key in keys:
+                print('{}: {}'.format(key, nums[key]))
         else:
-            return print_results(word_list, hot_list)
-
-    response = requests.get(URL, headers=HEADERS, params=params)
-    posts = response.json().get('data', {}).get('children', {})
-    if response.status_code != 200 or not posts:
-        return None
-    data = response.json().get('data', {})
-    after = data.get('after', 'STOP')
-    if not after:
-        after = "STOP"
-    hot_list = hot_list + [post.get('data', {}).get('title')
-                           for post in data.get('children', [])]
-    return count_words(subreddit, word_list, hot_list, after)
-
-
-def print_results(word_list, hot_list):
-    '''Prints'''
-    count = {}
-    for word in word_list:
-        count[word] = 0
-    for title in hot_list:
-        for word in word_list:
-            count[word] = count[word] +\
-             len(re.findall(r'(?:^| ){}(?:$| )'.format(word), title, re.I))
-
-    count = {k: v for k, v in count.items() if v > 0}
-    words = sorted(list(count.keys()))
-    for word in sorted(words,
-                       reverse=True, key=lambda k: count[k]):
-        print("{}: {}".format(word, count[word]))
+            count_words(subreddit, wordlist, nums, data['after'])
